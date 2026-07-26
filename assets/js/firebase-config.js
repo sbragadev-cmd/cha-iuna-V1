ba1e36006237f7fc67a41abce478fad6ce902414
sbragadev-cmd <sbraga.dev@gmail.com>
2026-07-26 23:16:46 +0000

    Atualizar firebase

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -16,12 +16,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_REAL",
+  apiKey: "AIzaSyDwn2pB-P8Vb-Z-qPGrpu7p01NTmxpa7Xs",
   authDomain: "iuna-e113d.firebaseapp.com",
   projectId: "iuna-e113d",
   storageBucket: "iuna-e113d.firebasestorage.app",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  messagingSenderId: "611255762922",
+  appId: "1:611255762922:web:be70b6fd48bff0c4db3c93"
 };
 
 const app = getApps().length
